3e3c266005ed5cf2f9d4f65febd8f0b8da89d816
kiyota <c4mo.sw@gmail.com>
2026-07-12 10:05:18 -0500

    fix(window): rules are modified

--- config/hypr/windowrules.lua
+++ config/hypr/windowrules.lua
@@ -4,27 +4,27 @@
 -- Example window rules that are useful
 
 local suppressMaximizeRule = hl.window_rule({
-    -- Ignore maximize requests from all apps. You'll probably like this.
-    name  = "suppress-maximize-events",
-    match = { class = ".*" },
+	-- Ignore maximize requests from all apps. You'll probably like this.
+	name = "suppress-maximize-events",
+	match = { class = ".*" },
 
-    suppress_event = "maximize",
+	suppress_event = "maximize",
 })
 -- suppressMaximizeRule:set_enabled(false)
 
 hl.window_rule({
-    -- Fix some dragging issues with XWayland
-    name  = "fix-xwayland-drags",
-    match = {
-        class      = "^$",
-        title      = "^$",
-        xwayland   = true,
-        float      = true,
-        fullscreen = false,
-        pin        = false,
-    },
+	-- Fix some dragging issues with XWayland
+	name = "fix-xwayland-drags",
+	match = {
+		class = "^$",
+		title = "^$",
+		xwayland = true,
+		float = true,
+		fullscreen = false,
+		pin = false,
+	},
 
-    no_focus = true,
+	no_focus = true,
 })
 
 -- Layer rules also return a handle.
@@ -37,16 +37,20 @@ hl.window_rule({
 
 -- Hyprland-run windowrule
 hl.window_rule({
-    name  = "move-hyprland-run",
-    match = { class = "hyprland-run" },
+	name = "move-hyprland-run",
+	match = { class = "hyprland-run" },
 
-    move  = "20 monitor_h-120",
-    float = true,
+	move = "20 monitor_h-120",
+	float = true,
 })
 
 hl.window_rule({ match = { class = "kitty" }, opacity = "0.9" })
-hl.window_rule({ match = { class = "firefox" }, opacity = "0.9" })
+hl.window_rule({ match = { class = "firefox" }, opacity = "1" })
 hl.window_rule({ match = { class = "Spotify" }, opacity = "0.9" })
-hl.window_rule({ match = { class = "yazi" }, opacity = "0.9", float = true, size = {1000, 600}, move = {"cursor_x-(window_w*0.5))", "(cursor_y-(window_h*0.5))"} })
-
-
+hl.window_rule({
+	match = { class = "yazi" },
+	opacity = "0.9",
+	float = true,
+	size = { 1000, 600 },
+	move = { "cursor_x-(window_w*0.5))", "(cursor_y-(window_h*0.5))" },
+})
